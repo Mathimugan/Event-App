14e5f3ffe962c3285d5d430790cdb50c4800c550
Mathimugan <mathi1960mugan@gmail.com>
2020-07-26 01:43:59 +0800

    loginout  file updated

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
 	session_start();
+	if(!isset($_SESSION['user_id']))
+{
+// not logged in
+header('Location: login.php');
+exit();
+}
 	session_destroy();
 	header("location:index.php");
 ?>
