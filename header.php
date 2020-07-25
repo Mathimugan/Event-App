9129969a1bee7ca3d00fcac53112e8c03db1b58e
Mathimugan <mathi1960mugan@gmail.com>
2020-07-25 11:07:57 +0800

    initial menubar created

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <title>Event App</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png"/>
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic%7CPlayball%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="vendors/bootstrap/css/bootstrap-theme.min.css">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" type="text/css" href="vendors/font-awesome/css/font-awesome.min.css">
+
+    <!-- Swiper -->
+    <link rel="stylesheet" type="text/css" href="vendors/swiper/css/swiper.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/welcome.css">
+</head>
+
+<?php ?>
