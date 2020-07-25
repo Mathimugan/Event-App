aaaccf87c7464c80a30b081655f42a2ef2b2cba8
Mathimugan <mathi1960mugan@gmail.com>
2020-07-25 12:23:57 +0800

    initial css uploaded

---
+++ menubar.php
@@ -0,0 +1,29 @@
+<?php ?>
+	<header id="header">
+    <div class="menu-container">
+        <div class="container">
+            <div class="row">
+                <div  class="col-md-7">
+                    <nav class="main-nav">
+                        <ul>
+                            <li>
+                                <a href="index.php">Home</a>
+                            </li>        
+                            <li><a href="about.php">About</a></li>
+                            <li class="current-menu-item"><a href="contact.php">Contact</a></li>
+                        </ul>
+                        <a href="javascript:;" id="close-menu"> <i class="fa fa-close"></i></a>
+                    </nav>
+                                            </div>
+                <div class=" col-md-5 h-search">
+                    <div class="head-social">
+                        <a class="socials" href="#">Username</a>
+                        <a class="socials" href="#">Logout</a>
+               
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+	<?php  ?>
