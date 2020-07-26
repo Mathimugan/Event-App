867f05789e7908458f4d48ec76f0bcb3cb29d72e
Mathimugan <mathi1960mugan@gmail.com>
2020-07-26 22:26:01 +0800

    db updated

--- dump/task.sql
+++ dump/task.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 26, 2020 at 04:05 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.0.26
+-- Host: localhost
+-- Generation Time: Jul 26, 2020 at 02:24 PM
+-- Server version: 5.7.28
+-- PHP Version: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,21 +32,25 @@ CREATE TABLE `bookings` (
   `event_id` int(100) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `created_date` date NOT NULL
+  `created_date` date NOT NULL,
+  `user_id` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bookings`
 --
 
-INSERT INTO `bookings` (`booking_id`, `event_id`, `name`, `email`, `created_date`) VALUES
-(1, 22, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25'),
-(2, 21, 'undefined', 'undefined', '2020-07-25'),
-(3, 21, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25'),
-(4, 18, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25'),
-(5, 21, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25'),
-(6, 19, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25'),
-(7, 24, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-26');
+INSERT INTO `bookings` (`booking_id`, `event_id`, `name`, `email`, `created_date`, `user_id`) VALUES
+(1, 22, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25', 0),
+(2, 21, 'undefined', 'undefined', '2020-07-25', 0),
+(3, 21, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25', 0),
+(4, 18, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25', 0),
+(5, 21, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25', 0),
+(6, 19, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-25', 0),
+(7, 24, 'MathiMugan', 'mathimugancse2010@gmail.com', '2020-07-26', 0),
+(8, 39, 'Demo', 'demo@gmail.com', '2020-07-26', 2),
+(9, 40, 'Push', 'pushpa@gmail.com', '2020-07-26', 3),
+(10, 38, '', '', '2020-07-26', 3);
 
 -- --------------------------------------------------------
 
@@ -60,9 +63,9 @@ CREATE TABLE `events` (
   `event_title` varchar(255) NOT NULL,
   `event_date` date NOT NULL,
   `event_description` text NOT NULL,
-  `attachment` text NOT NULL,
-  `refer_name` text NOT NULL,
-  `upload_datetime` datetime NOT NULL,
+  `attachment` text,
+  `refer_name` text,
+  `upload_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `event_time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -71,10 +74,8 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`event_id`, `event_title`, `event_date`, `event_description`, `attachment`, `refer_name`, `upload_datetime`, `event_time`) VALUES
-(28, 'Chinese New Year @ Suntec Citys', '2020-07-08', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'c77cfd8a6d3845e3954e87ac5f68ac67.jpg', '285f1ce89bb2ba65.14481853.jpg', '2020-07-26 10:21:15', '12:20:00'),
-(30, 'Year End Celebration', '2020-11-30', 'Tesgdgsgsgsgssg', '2cdc1f968156e8cdf3b62f5ef7e72311.jpg', '305f1d00ca39b2d4.49475956.jpg', '2020-07-26 12:04:26', '18:00:00'),
-(31, 'Gsgsgs', '2020-07-26', 'This is new text', 'original.jpg', '315f1d030a0c8c64.90863256.jpg', '2020-07-26 12:14:02', '18:13:00'),
-(32, 'Dgsg', '2020-07-28', 'Sfstsfsgsfdgdgdgdgsgsfsfsfsgsgsgsgsgsgs\r\nggsgsgs', '30945067690_0a9a35f210_b.jpg', '325f1d03ebc06ae5.80757461.jpg', '2020-07-26 12:17:47', '18:17:00');
+(38, 'New year celebration@  Suntech City', '2020-12-31', 'Zggsgsgs', 'Palani-Dhandayudhapaniswamy11.jpg', '385f1d5003d60d32.86483280.jpg', '2020-07-26 09:42:27', '06:00:00'),
+(40, 'Testing', '2020-07-26', 'Testing has been done', 'unnamed.jpg', '405f1d6a39e4bf49.21772582.jpg', '2020-07-26 11:34:17', '19:33:00');
 
 -- --------------------------------------------------------
 
@@ -96,7 +97,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `name`, `password`, `created_date`, `status`, `email_id`) VALUES
-(1, 'Admin', '$2y$10$w7AnuOSnMfOz0NzR3CURRuNAhR/w5tMW3aTH665Qfw1klvh2vFtAG', '2020-07-25', 'Active', 'admin@gmail.com');
+(1, 'Admin', '$2y$10$w7AnuOSnMfOz0NzR3CURRuNAhR/w5tMW3aTH665Qfw1klvh2vFtAG', '2020-07-25', 'Active', 'admin@gmail.com'),
+(2, 'Demo', '$2y$10$TtT8QAm405xYF6td4X.RBeiOE9lTA8.THRWop0nrNO9.1opEI20mm', '2020-07-26', 'Active', 'demo@gmail.com'),
+(3, 'Push', '$2y$10$VD7Pz3/UUSoOlJoUrd7qRuayFYXdn.HyGheqSrpZfL9OYqnjNBvjW', '2020-07-26', 'Active', 'pushpa@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -118,7 +121,9 @@ CREATE TABLE `user_group` (
 --
 
 INSERT INTO `user_group` (`group_id`, `group_name`, `user_view`, `user_edit`, `user_delete`, `user_id`) VALUES
-(1, 'Admin', 1, 1, 1, 1);
+(1, 'Admin', 1, 1, 1, 1),
+(2, 'User', 0, 0, 0, 2),
+(3, 'User', 0, 0, 0, 3);
 
 --
 -- Indexes for dumped tables
@@ -137,6 +142,12 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`event_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Indexes for table `user_group`
 --
 ALTER TABLE `user_group`
@@ -150,19 +161,25 @@ ALTER TABLE `user_group`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `booking_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `booking_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `event_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `event_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user_group`
 --
 ALTER TABLE `user_group`
-  MODIFY `group_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `group_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
